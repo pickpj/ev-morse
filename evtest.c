2f7b4392fcf321a494324c94663bfad320fcdce5
picklepissjar <118209356+picklepissjar@users.noreply.github.com>
2023-04-05 15:08:19 -0400

    Add files via upload

--- evtest.c
+++ evtest.c
@@ -27,27 +27,77 @@ static void run_hold() {
     int len = strlen(pattern);
     // vvv Your code goes here vvv
     switch (keyc) {
-        case 34:
-            system("playerctl --player=spotify,chromium,cmus,%%any play-pause");
+        case 164:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any play-pause");
+            memset(pattern, 0, sizeof(pattern));
+            break;
+        case 163:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any position 5+");
+            memset(pattern, 0, sizeof(pattern));
+            break;
+        case 165:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any position 5-");
+            memset(pattern, 0, sizeof(pattern));
             break;
     }
     // ^^^ Your code goes here ^^^
-    memset(pattern, 0, sizeof(pattern));
 }
 
 static void run_pattern() {
     int len = strlen(pattern);
     // vvv Your code goes here vvv
-    switch (keyc) {
-        case 34:
+    if (len >= 1) {
+        switch (keyc) {
+        case 96:
+            break;
+        case 164:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any play-pause");
+            break;
+        case 163:
+            for(int i = 0; i < len; i++) {
+                system("playerctl --player=spotify,youtube-music,cmus,%%any next");
+            } 
+            break;
+        case 165:
+            for(int i = 0; i < len; i++) {
+                system("playerctl --player=spotify,youtube-music,cmus,%%any previous");
+            } 
+            break;
+        case 74:
             if (strcmp(pattern,"0") == 0){
-                system("playerctl --player=spotify,chromium,cmus,%%any previous");
+                system("playerctl -p spotify volume 0.5 || playerctl -p youtube-music volume 5 || playerctl --player=cmus,%%any volume 0.5");
             } else if (strcmp(pattern,"1") == 0) {
-                system("playerctl --player=spotify,chromium,cmus,%%any next");
+                system("playerctl -p spotify volume 0.7 || playerctl -p youtube-music volume 15 || playerctl --player=cmus,%%any volume 0.7");
             } else {
-                system("playerctl --player=spotify,chromium,cmus,%%any position 3+");
+                system("playerctl -p spotify volume 1 || playerctl -p youtube-music volume 30 || playerctl --player=cmus,%%any volume 1");
             }
             break;
+        case 41:
+            if (strcmp(pattern, "00") == 0) {
+                system("xdotool key --delay 1 asciitilde");
+            } else if (strcmp(pattern, "1") == 0) {
+                system("gnome-pie -o 272");
+            } else {
+                for(int i = 0; i < len; i++) {
+                    strcat(grave, "`");
+                } 
+                sprintf(pattern, "xdotool type '%s'", grave);
+                system(pattern);
+                memset(grave, 0, sizeof(grave));
+            }
+            break;
+        case 65:
+            fp = popen("xdotool getmouselocation", "r");
+            fgets(mpos, sizeof(mpos)-1, fp);
+            reti = regcomp(&regex, mregex, REG_EXTENDED);
+            reti = regexec(&regex, mpos, nmatch, pmatch, 0);
+            pclose(fp);
+            system("xdotool mousemove 1300 1050 && xdotool click 1");
+            sprintf(pattern, "xdotool mousemove %.*s %.*s", (int)(pmatch[1].rm_eo - pmatch[1].rm_so), mpos + pmatch[1].rm_so, (int)(pmatch[2].rm_eo - pmatch[2].rm_so), mpos + pmatch[2].rm_so);
+            system(pattern);
+            regfree(&regex);
+            break;
+        }
     }
     // ^^^ Your code goes here ^^^
     memset(pattern, 0, sizeof(pattern));
@@ -103,7 +153,6 @@ int main(int argc, char **argv)
                     strcat(pattern,"1");
                     hold++;
                 } else if (hold % interval == (interval - 1)) {
-                    memset(pattern, 0, sizeof(pattern));
                     run_hold();
                     hold ++;
                 } else {
@@ -112,7 +161,7 @@ int main(int argc, char **argv)
             } else if (ev.value == 1) {
                 hold = 0;
             } else if (ev.value == 0) {
-                if (hold > interval) {
+                if (strlen(pattern) == 0 && hold > interval) {
                     hold = 0;
                     printf("Key code: %d, Value: %s \n", ev.code, pattern);
                     fflush(stdout);
