c1f1fe36624abf0535da6b5ecbb78a6571d6ece0
picklepissjar <118209356+picklepissjar@users.noreply.github.com>
2023-04-28 10:04:27 -0400

    Modifiers

--- evmorse.c
+++ evmorse.c
@@ -7,40 +7,116 @@
 // #include <linux/input.h>
 #include <linux/uinput.h>
 
+#include <regex.h>
+
 #define ANSI_RED     "\x1b[31m"
 #define ANSI_RESET   "\x1b[0m"
 
-
+// main variables
 char pattern[100] = "";
 int keyc = -1;
 int nlstate;
+int shiftstate = 0;
+int ctrlstate = 0;
+int altstate = 0;
 
+// user variables
+char grave[10] = "";
+
+FILE* fp;
+char mpos[50];
+regex_t regex;
+int reti;
+const char *mregex = "x:([0-9]+) y:([0-9]+)";
+size_t nmatch = 3;
+regmatch_t pmatch[3];
+
+// User defined functions run_hold and run_pattern
 static void run_hold() {
     int len = strlen(pattern);
-    //! vvv Your code goes here vvv
-
-    //! ^^^ Your code goes here ^^^
+    // vvv Your code goes here vvv
+    switch (keyc) {
+        case 164:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any play-pause");
+            memset(pattern, 0, sizeof(pattern));
+            break;
+        case 163:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any position 5+");
+            memset(pattern, 0, sizeof(pattern));
+            break;
+        case 165:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any position 5-");
+            memset(pattern, 0, sizeof(pattern));
+            break;
+    }
+    // ^^^ Your code goes here ^^^
 }
 
 static void run_pattern() {
     int len = strlen(pattern);
     if (len >= 1) {
-    //! vvv Your code goes here vvv
-
-    //! ^^^ Your code goes here ^^^
+    // vvv Your code goes here vvv
+        switch (keyc) {
+        case 96:
+            break;
+        case 164:
+            system("playerctl --player=spotify,youtube-music,cmus,%%any play-pause");
+            break;
+        case 163:
+            for(int i = 0; i < len; i++) {
+                system("playerctl --player=spotify,youtube-music,cmus,%%any next");
+            } 
+            break;
+        case 165:
+            for(int i = 0; i < len; i++) {
+                system("playerctl --player=spotify,youtube-music,cmus,%%any previous");
+            } 
+            break;
+        case 74:
+            if (strcmp(pattern,"0") == 0){
+                system("playerctl -p spotify volume 0.5 || playerctl -p youtube-music volume 5 || playerctl --player=cmus,%%any volume 0.5");
+            } else if (strcmp(pattern,"1") == 0) {
+                system("playerctl -p spotify volume 0.7 || playerctl -p youtube-music volume 20 || playerctl --player=cmus,%%any volume 0.7");
+            } else if (len == 2){
+                system("playerctl -p spotify volume 1 || playerctl -p youtube-music volume 50 || playerctl --player=cmus,%%any volume 1");
+            } else {
+                system("playerctl -p spotify volume 1 || playerctl -p youtube-music volume 100 || playerctl --player=cmus,%%any volume 1");
+            }
+            break;
+        case 41:
+            if (strcmp(pattern, "00") == 0) {
+                system("xdotool key --delay 1 asciitilde");
+            } else if (strcmp(pattern, "1") == 0) {
+                system("gnome-pie -o 272");
+            } else {
+                for(int i = 0; i < len; i++) {
+                    strcat(grave, "`");
+                } 
+                sprintf(pattern, "xdotool type '%s'", grave);
+                system(pattern);
+                memset(grave, 0, sizeof(grave));
+            }
+            break;
+        case 65:
+            fp = popen("xdotool getmouselocation", "r");
+            fgets(mpos, sizeof(mpos)-1, fp);
+            reti = regcomp(&regex, mregex, REG_EXTENDED);
+            reti = regexec(&regex, mpos, nmatch, pmatch, 0);
+            pclose(fp);
+            system("xdotool mousemove 1300 1050 && xdotool click 1");
+            sprintf(pattern, "xdotool mousemove %.*s %.*s", (int)(pmatch[1].rm_eo - pmatch[1].rm_so), mpos + pmatch[1].rm_so, (int)(pmatch[2].rm_eo - pmatch[2].rm_so), mpos + pmatch[2].rm_so);
+            system(pattern);
+            regfree(&regex);
+            break;
+        }
     }
+    // ^^^ Your code goes here ^^^
     memset(pattern, 0, sizeof(pattern));
 }
 
-
-static void emit(int fd, int type, int code, int val) {
-    struct input_event ie;
-    ie.type = type;
-    ie.code = code;
-    ie.value = val;
-
-    write(fd, &ie, sizeof(ie));
-}
+// main function declarations 
+static void emit(int fd, int type, int code, int val);
+static void setmodifier(int keycode, int value);
 
 int main(int argc, char **argv)
 {
@@ -122,48 +198,103 @@ int main(int argc, char **argv)
             break;
         }
         if (ev.type == EV_KEY) {
-            if (keyc != ev.code && keyc != -1) {
+            if (ev.code == 97 | ev.code == 29 | ev.code == 54 | ev.code == 42 | ev.code == 100 | ev.code == 56) {
+                if (ev.value != 2) {
+                    setmodifier(ev.code, ev.value);
+                }
+            } else if (keyc != ev.code && keyc != -1) {
                 memset(pattern, 0, sizeof(pattern));
-            }
-            keyc = ev.code;
-            
-            if (ev.value == 2) {
-                if (hold < 1){
-                    strcat(pattern,"1");
-                    hold++;
-                } else if (hold % interval == (interval - 1)) {
-                    run_hold();
-                    hold ++;
-                } else {
-                    hold ++;
-                }
-            } else if (ev.value == 1) {
-                hold = 0;
-            } else if (ev.value == 0) {
-                if (keyc == 69) {
-                    nlstate = !nlstate;
-                    printf("Numlock State %d \n", nlstate);
-                }
-                if (strlen(pattern) == 0 && hold > interval) {
+                keyc = ev.code;
+            } else {
+                keyc = ev.code;
+                if (ev.value == 2) {
+                    if (hold < 1){
+                        strcat(pattern,"1");
+                        hold++;
+                    } else if (hold % interval == (interval - 1)) {
+                        run_hold();
+                        hold ++;
+                    } else {
+                        hold ++;
+                    }
+                } else if (ev.value == 1) {
                     hold = 0;
-                    printf("Key code: %d, Value: %s \n", ev.code, pattern);
-                    fflush(stdout);
-                    memset(pattern, 0, sizeof(pattern));
-                } else if (hold > 0) {
-                    hold = 0;
-                    printf("Key code: %d, Value: %s \n", ev.code, pattern);
-                    fflush(stdout);
-                } else {
-                    strcat(pattern,"0");
-                    printf("Key code: %d, Value: %s \n", ev.code, pattern);
-                    fflush(stdout);
+                } else if (ev.value == 0) {
+                    if (keyc == 69) {
+                        nlstate = !nlstate;
+                        printf("Numlock State %d \n", nlstate);
+                    }
+                    if (strlen(pattern) == 0 && hold > interval) {
+                        hold = 0;
+                        printf("Key code: %d, Value: %s \n", ev.code, pattern);
+                        fflush(stdout);
+                        memset(pattern, 0, sizeof(pattern));
+                    } else if (hold > 0) {
+                        hold = 0;
+                        printf("Key code: %d, Value: %s \n", ev.code, pattern);
+                        fflush(stdout);
+                    } else {
+                        strcat(pattern,"0");
+                        printf("Key code: %d, Value: %s \n", ev.code, pattern);
+                        fflush(stdout);
+                    }
                 }
-            }
-        
+
             setitimer(ITIMER_REAL, &timer, NULL);
+            }
         }
     }
     close(fd);
     return 0;
 }
 
+static void setmodifier(int keycode, int value) {
+    switch (keycode) {
+        case 42: //lshift
+        case 54: //rshift
+            if (value == 1) {
+                shiftstate = 1;
+                printf("shift on\n");
+                fflush(stdout);
+            } else {
+                shiftstate = 0;
+                printf("shift off\n");
+                fflush(stdout);
+            }
+            break;
+        case 29: //lctrl
+        case 97: //rctrl
+            if (value == 1) {
+                ctrlstate = 1;
+                printf("ctrl on\n");
+                fflush(stdout);
+            } else {
+                ctrlstate = 0;
+                printf("ctrl off\n");
+                fflush(stdout);
+            }
+            break;
+        case 56: //lalt
+        case 100: //ralt
+            if (value == 1) {
+                altstate = 1;
+                printf("alt on\n");
+                fflush(stdout);
+            } else {
+                altstate = 0;
+                printf("alt off\n");
+                fflush(stdout);
+            }
+            break;
+    }
+}
+
+// Functions used by the main loop
+static void emit(int fd, int type, int code, int val) {
+    struct input_event ie;
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+
+    write(fd, &ie, sizeof(ie));
+}
