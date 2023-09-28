c4d5f197d671e24220532251e63c2281d91c8abe
pickpj <118209356+pickpj@users.noreply.github.com>
2023-09-28 15:19:22 -0400

    Retains elevated privilege for reopening devices
    Commands now execute in a fork with reduced privilege.

--- headphone.c
+++ headphone.c
@@ -5,12 +5,13 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <libevdev/libevdev.h>
 #include <libevdev/libevdev-uinput.h>
 
-#include <regex.h>
-
 #define ANSI_RED     "\x1b[31m"
+#define ANSI_GREEN   "\x1b[32m"
 #define ANSI_RESET   "\x1b[0m"
 
 // main variables
@@ -23,14 +24,12 @@ int altstate = 0;
 
 int fd;
 struct libevdev_uinput *uidev;
+int arguid;
 
-static void evdev_write(int keyp) {
-    libevdev_uinput_write_event (uidev, EV_KEY, keyp, 1);
-    libevdev_uinput_write_event (uidev, EV_SYN, SYN_REPORT, 0);
-    libevdev_uinput_write_event (uidev, EV_KEY, keyp, 0);
-    libevdev_uinput_write_event (uidev, EV_SYN, SYN_REPORT, 0);
-}
-// User defined functions run_hold and run_patternx
+static void evdev_write(int keyp);
+static void run_non_alrm(int uid, char *func);
+
+// User defined functions run_hold and run_pattern
 static void run_down() {
     switch (keyc) {
         case 200:
@@ -55,14 +54,30 @@ static void run_hold() {
 }
 
 static void run_pattern() {
-    int len = strlen(pattern);
-    if (len >= 1) {
-    //! vvv Your code goes here vvv
-        switch (keyc) {
+    int status;
+    int child = fork();
+    if (child == 0) {
+        if (setuid(arguid) == -1) {
+            perror("setuid");
+            _exit(1);
         }
+        int len = strlen(pattern);
+        if (len >= 1) {
+        //! vvv Your code goes here vvv
+            switch (keyc) {
+            }
+        //! ^^^ Your code goes here ^^^
+        }
+        kill(child, SIGTERM);
+        return;
+    } else if (child > 0) {
+        memset(pattern, 0, sizeof(pattern));
+        waitpid(child,&status,WUNTRACED | WCONTINUED);
+        return;
+    } else {
+        perror("fork");
+        _exit(1);
     }
-    //! ^^^ Your code goes here ^^^
-    memset(pattern, 0, sizeof(pattern));
 }
 
 // Function declarations for main (Found near the end)
@@ -74,7 +89,7 @@ int main(int argc, char **argv)
     // Check for proper input of command
     struct input_event ev;
     if (argc < 3) {
-        printf(ANSI_RED "Usage: " ANSI_RESET "sudo %s $(id -u) <input device>\n", argv[0]);
+        printf(ANSI_RED "Usage: " ANSI_RESET "sudo -E %s $(id -u) <input device>\n", argv[0]);
         return 1;
     }
     fd = open(argv[2], O_RDONLY);
@@ -92,7 +107,7 @@ int main(int argc, char **argv)
     timer.it_value.tv_usec = 300000;
 
     //^ Timing between key held action (30ms * interval)
-    int interval = 7;
+    int interval = 10;
 
     // Set signal to run the function: run_pattern
     signal(SIGALRM, run_pattern);
@@ -101,11 +116,7 @@ int main(int argc, char **argv)
 
     // Changing uid so that commands execute as the user (defined by '$(id -u)' ; important for interactions with playerctl)
     // Could be removed to run all commands as sudo
-    int argid = atoi(argv[1]);
-    if (setuid(argid) == -1) {
-        perror("setuid");
-        return 1;
-    }
+    arguid = atoi(argv[1]);
 
     // Creates a "fake" device that sends 2 numlock key presses
     // This is to determine whether numlock is on/off
@@ -114,7 +125,6 @@ int main(int argc, char **argv)
     dev = libevdev_new ();
     libevdev_set_name (dev, "ev-headphone");
 
-    // This device is only able to send a numlock key
     libevdev_enable_event_type(dev, EV_KEY);
 
     libevdev_enable_event_code(dev, EV_KEY, KEY_VOLUMEUP, NULL);
@@ -125,7 +135,10 @@ int main(int argc, char **argv)
     if (err != 0) {
         return err;
     }
-    sleep(1);    
+
+    // Device is destroyed upon sigint signal, for those who would like to the virtual keyboard to send input
+    // This method can be faster than xdotool for more niche key characters
+    // Device can be destroyed here if desired, but comment out the same line in the exitsignal() function
 
     // libevdev_uinput_destroy (uidev);
 
@@ -144,7 +157,7 @@ int main(int argc, char **argv)
             } else if (keyc != ev.code && keyc != -1) {
                 memset(pattern, 0, sizeof(pattern));
                 keyc = ev.code;
-                run_down();
+                run_non_alrm(arguid, "down");
             } else {
                 keyc = ev.code;
                 // value 2; key held down
@@ -153,7 +166,7 @@ int main(int argc, char **argv)
                         strcat(pattern,"1");
                         hold++;
                     } else if ((hold-5) % interval == (interval - 1)) {
-                        run_hold();
+                        run_non_alrm(arguid, "hold");
                         hold ++;
                     } else {
                         hold ++;
@@ -161,7 +174,7 @@ int main(int argc, char **argv)
                 // value 1; key down
                 } else if (ev.value == 1) {
                     hold = 0;
-                    run_down();
+                    run_non_alrm(arguid, "down");
                 // value 0; key up
                 } else if (ev.value == 0) {
                     if (strlen(pattern) == 0 && hold > interval) {
@@ -188,11 +201,63 @@ int main(int argc, char **argv)
                 printf("Numlock State %d\n",nlstate);
                 fflush(stdout);
             }
+        } else if (access(argv[2], F_OK) != 0) {
+            printf(ANSI_RED "Lost device: " ANSI_RESET "waiting for reconnect\n");
+            fflush(stdout);
+            while (access(argv[2], F_OK) != 0) {
+                usleep(500000); //0.5s
+            }
+
+            fd = open(argv[2], O_RDONLY);
+            if (fd < 0) {
+                perror("Failed to reopen device");
+                return 1;
+            }
+            printf(ANSI_GREEN "Device reconnected\n" ANSI_RESET);
+            fflush(stdout);
         }
     }
 }
 
-// Functions used by the main loop
+// Extra functions
+
+static void run_non_alrm(int uid, char *func) {
+    int status;
+    int child = fork();
+    if (child == 0) {
+        if (setuid(uid) == -1) {
+            perror("setuid");
+            exit(1);
+        }
+        if (!strcmp(func, "hold")) {
+            run_hold();
+            memset(pattern, 0, sizeof(pattern));
+        } else if (!strcmp(func, "down")) {
+            run_down();
+        } else {
+            perror("run_non_alrm");
+        }
+        kill(child, SIGTERM);
+        return;
+    } else if (child > 0) {
+        if (!strcmp(func, "hold")) {
+            memset(pattern, 0, sizeof(pattern));            
+        }
+        waitpid(child,&status,WUNTRACED | WCONTINUED | WIFSIGNALED(status));
+        return;
+    } else {
+        perror("fork");
+        exit(1);
+    }
+}
+
+static void evdev_write(int keyp) {
+    libevdev_uinput_write_event (uidev, EV_KEY, keyp, 1);
+    libevdev_uinput_write_event (uidev, EV_SYN, SYN_REPORT, 0);
+    libevdev_uinput_write_event (uidev, EV_KEY, keyp, 0);
+    libevdev_uinput_write_event (uidev, EV_SYN, SYN_REPORT, 0);
+}
+
 static void setmodifier(int keycode, int value) {
     switch (keycode) {
         case 42: //lshift
@@ -217,7 +282,7 @@ static void setmodifier(int keycode, int value) {
 }
 
 static void exitsignal() {
-    printf("SIGINT signal recieved\n");
+    printf("SIGINT signal recieved");
     libevdev_uinput_destroy (uidev);
     close(fd);
     exit(0);
